fc63a0dedcf3167dca90d61801c926ebd5956962
Shreykumar Makwana <TMDJ64@motorolasolutions.com>
2022-10-28 13:19:16 +0530

    Intial Setup Commit

---
+++ contracts/dapp-token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+contract DcentToken{
+    // Constructor
+    // Set the total number of tokens
+    // Read the total number of tokens
+    uint256 public totalSupply;
+
+    constructor(){
+        totalSupply = 7000000;
+    }
+}
